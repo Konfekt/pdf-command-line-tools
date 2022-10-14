813e0c1f6bfea05021e6a3c2d745d2e442f76f3c
Konfekt <Konfekt@users.noreply.github.com>
2022-10-14 22:24:53 +0200

    html2pdf: use htmldoc as second-best alternative to wkhtmltopdf

--- html2pdf.sh
+++ html2pdf.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-command -v wkhtmltopdf >/dev/null 2>&1 || exit 1
-
-if command -v parallel >/dev/null 2>&1; then
-  parallel \
-    wkhtmltopdf {} {.}.pdf \
-    ::: "$@";
+if command -v wkhtmltopdf >/dev/null 2>&1; then
+  if command -v parallel >/dev/null 2>&1; then
+    parallel \
+      wkhtmltopdf {} {.}.pdf \
+      ::: "$@";
+  else
+    for f in "$@"; do
+      wkhtmltopdf "$f" "${f%.[^.]*}".pdf
+    done
+  fi
+elif command -v htmldoc > /dev/null 2>&1; then
+  	if command -v parallel > /dev/null 2>&1; then
+    	parallel \
+				 htmldoc --cont --headfootsize 8.0 --linkcolor blue --linkstyle plain --format pdf14 {} > {.}.pdf \
+      	::: "$@"
+  	else
+    	for f in "$@"; do
+        htmldoc --cont --headfootsize 8.0 --linkcolor blue --linkstyle plain --format pdf14 --outfile "${f%.[^.]*}".pdf "$f" 
+    	done
+  	fi
 else
-  for f in "$@"; do
-    wkhtmltopdf "$f" "${f%.[^.]*}".pdf
-  done
+  exit 1
 fi
+
