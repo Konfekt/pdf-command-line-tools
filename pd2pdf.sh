36437b1265da1eb42eb38b5cb12edf1af849dbf1
Konfekt <Konfekt@users.noreply.github.com>
2022-10-18 06:27:01 +0200

    pd2pdf: allow for initial pandoc parameters

--- pd2pdf.sh
+++ pd2pdf.sh
@@ -3,9 +3,18 @@
 command -v pandoc >/dev/null 2>&1 || exit 1
 
 if [ $# -eq 0 ]; then
-  echo "usage: ${0} <file> ..."
+  echo "usage: ${0} [pandoc options] <file> [another file]..."
   return 2
 fi
+
+PANDOC_OPTIONS="--standalone"
+
+for p in "$@"; do
+  test -f "$p" && break
+
+  PANDOC_OPTIONS="$PANDOC_OPTIONS $p"
+  shift
+done
 
 case "${1}" in
   (*.md) from=markdown ;;
@@ -20,18 +29,18 @@ if [ $# -eq 1 ]; then
   eval in_file=\${$#}
   if command -v latexrun >/dev/null 2>&1; then
     tex_file="${in_file%.[^.]*}".tex
-    pandoc --standalone --from $from --to latex --output "$tex_file" "$in_file" &&
+    pandoc $PANDOC_OPTIONS --from $from --to latex --output "$tex_file" "$in_file" &&
 	  latexrun "$in_file"
 	else
     out_file="${in_file%.[^.]*}".pdf
-    pandoc --standalone --from $from --to latex --output "$out_file" "$in_file"
+    pandoc $PANDOC_OPTIONS --from $from --to latex --output "$out_file" "$in_file"
 	fi
 else
   if command -v parallel >/dev/null 2>&1; then
-    parallel pandoc --standalone --from $from --to latex --output {.}.pdf {} ::: "$@";
+    parallel pandoc $PANDOC_OPTIONS --from $from --to latex --output {.}.pdf {} ::: "$@";
   else
     for f in "$@"; do
-      pandoc --standalone --from $from --to latex --output "${f%.[^.]*}".pdf "$f"
+      pandoc $PANDOC_OPTIONS --from $from --to latex --output "${f%.[^.]*}".pdf "$f"
     done
   fi
 fi
