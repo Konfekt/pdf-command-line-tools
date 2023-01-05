0d514eb545c9d14d5adcadf09d1a210403075e96
Konfekt <Konfekt@users.noreply.github.com>
2023-01-05 09:29:17 +0100

    pdf-deflate: only convert *.PDF files
    Otherwise perfectly fine files of other file types are replaced by garbage...

--- pdf-deflate.sh
+++ pdf-deflate.sh
@@ -11,8 +11,27 @@ export threshold=80
 # default > ebook > screen 
 export quality="default"
 
-deflate() {
+fileext () {
+	case "$1" in
+		.*.*) extension=${1##*.};;
+		.*) extension=;;
+		*.*) extension=${1##*.};;
+	esac
+	echo "$extension"
+} && export -f fileext
+
+cleanup () {
+    [ -f "$1.new" ] && rm "$1.new"
+}
+trap cleanup EXIT
+
+deflate () {
     f="$1"
+    fileext="$(fileext "$f")"
+    if [ "$fileext" != "pdf" ]; then
+        echo "$f does not seem to be a PDF file. Exiting."
+        return 1
+    fi
     fnew="$f.new"
     echo "Compressing $f ..."
     ps2pdf \
@@ -26,18 +45,24 @@ deflate() {
         # -dColorImageDownsampleType=/Bicubic -dColorImageResolution=72 \
         # -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=72 \
         # -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=72 \
-    old_size=$(stat --printf="%s" "$f")
-    new_size=$(stat --printf="%s" "$fnew")
-    percent=$((new_size * 100 / old_size))
-    echo -n "New size: $percent%. "
+    if [ $? -eq 0 ]; then
+        old_size=$(stat --printf="%s" "$f")
+        new_size=$(stat --printf="%s" "$fnew")
+        percent=$((new_size * 100 / old_size))
+        echo -n "New size: $percent%. "
 
-    if [ $percent -gt $threshold ]; then
-        echo "Keeping original."
-        rm "$fnew"
-        return
+        if [ $percent -gt $threshold ]; then
+            echo "Keeping original."
+            rm "$fnew"
+            return
+        else
+            echo "Replacing file."
+            mv "$fnew" "$f"
+        fi
     else
-        echo "Replacing file."
-        mv "$fnew" "$f"
+        echo "Error occurred. Keeping original."
+        rm "$fnew"
+        return 1
     fi
 } && export -f deflate
 
